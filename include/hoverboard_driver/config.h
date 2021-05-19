8a9f4bc99c4e5a13d5447a74df6ea0d4ab8b96cf
martin nievas <martin.nievas.ar@gmail.com>
2021-05-19 18:36:57 -0300

    Incremental changes are a construcción social...

--- include/hoverboard_driver/config.h
+++ include/hoverboard_driver/config.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#define PORT "/dev/ttyUSB0"
 #define DIRECTION_CORRECTION 1
-#define WHEEL_RADIUS 0.0825
+
+// PWM control method seems to be much more accurate
+#define CONTROL_PWM
+// #define CONTROL_SPEED
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,19 @@
 int main(int argc, char **argv) {
     ros::init(argc, argv, "hoverboard_driver");
 
-    Hoverboard hoverboard;
+    Hoverboard& hoverboard = Hoverboard::getInstance();
     controller_manager::ControllerManager cm(&hoverboard);
 
     ros::AsyncSpinner spinner(1);
     spinner.start();
 
     ros::Time prev_time = ros::Time::now();
-    ros::Rate rate(50.0);
+
+    float loop_hz;
+    ros::param::get("/robot/hardware_interface/loop_hz", loop_hz);
+    ROS_INFO("main loop rate [Hz]: %f", loop_hz);
+
+    ros::Rate rate(loop_hz);
 
     while (ros::ok()) {
         const ros::Time time = ros::Time::now();
